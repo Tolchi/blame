fdce647f57be7d09d93279573311c2c43ae0c509
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-03-19 11:43:14 -0300

    spec fixed

--- spec/requests/auth_logins_spec.rb
+++ spec/requests/auth_logins_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
+require 'debugger'
+
 describe "AuthLogins" do
   it "should let user completes it's mail pastword" do
-    visit( login_path )
-    fill_in( :em, :with => 'diego.sanches@gmail.com' )
-    fill_in( :pass, :with => '12345' )
+    visit( login_page_path )
+    debugger
+    page.fill_in( 'em', :with => 'diego.sanches@gmail.com' )
+    page.fill_in( 'pass', :with => '12345' )
     click_button( 'log in' )
     page.should( page.has_content?( 'diego.sanches@gmail.com' ) )
   end
